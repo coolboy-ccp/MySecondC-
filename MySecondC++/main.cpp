3d5403088991e7950c77a6795110c16ef163f083
brian-chu <chuchengpeng@liqunfeideMac-mini.local>
2015-08-14 10:07:46 +0800

    Initial Commit

---
+++ MySecondC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MySecondC++
+//
+//  Created by liqunfei on 15/8/14.
+//  Copyright (c) 2015年 chuchengpeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
